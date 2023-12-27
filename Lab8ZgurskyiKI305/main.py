2432ea2180ee3725e2be1b0ef1966c89ef32e5a7
taras1o <88719171+taras1o@users.noreply.github.com>
2023-12-27 18:30:04 +0200

    Add files via upload

---
+++ Lab8ZgurskyiKI305/main.py
@@ -0,0 +1,58 @@
+import os
+import struct
+import sys
+import math
+
+def writeResTxt(fName, result):
+    with open(fName, 'w') as f:
+        f.write(str(result))
+
+def readResTxt(fName):
+    result = 0.0
+    try:
+        if os.path.exists(fName):
+            with open(fName, 'r') as f:
+                result = float(f.read())
+        else:
+            raise FileNotFoundError(f"File {fName} not found.")
+    except FileNotFoundError as e:
+        print(e)
+    return result
+
+def writeResBin(fName, result):
+    with open(fName, 'wb') as f:
+        # See https://docs.python.org/3/library/struct.html
+        f.write(struct.pack('d', result))
+
+def readResBin(fName):
+    result = 0.0
+    try:
+        if os.path.exists(fName):
+            with open(fName, 'rb') as f:
+                result = struct.unpack('d', f.read())[0]
+        else:
+            raise FileNotFoundError(f"File {fName} not found.")
+    except FileNotFoundError as e:
+        print(e)
+    return result
+
+def calculate(x):
+    try:
+        result = math.tan(x) / math.atan(x)
+    except ZeroDivisionError:
+        print("Error: Division by zero.")
+        sys.exit(1)
+    return result
+
+if __name__ == "__main__":
+    data = float(input("Enter data: "))
+    result = calculate(data)
+    print(f"Result is: {result}")
+    try:
+        writeResTxt("textRes.txt", result)
+        writeResBin("binRes.bin", result)
+        print("Result is: {0}".format(readResBin("binRes.bin")))
+        print("Result is: {0}".format(readResTxt("textRes.txt")))
+    except FileNotFoundError as e:
+        print(e)
+        sys.exit(1)
